f6e285ca8462f9196e6ca8dcc530ad47d403668a
StormRobert <stormarasa2@gmail.com>
2024-09-23 18:51:49 +0300

    small edits

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,20 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Homecards from './components/Homecards'
 import JobsListings from './components/JobsListings'
+import ViewAllJobs from './components/ViewAllJobs'
 
 const App = () => {
   return (
    <>
    <Navbar />
-   
-
     {/* <!-- Hero --> */}
     <Hero />
-
     {/* <!-- Developers and Employers --> */}
     <Homecards />
      {/* <!-- Browse Jobs --> */}
-     <JobsListings />
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+    <JobsListings />
+    {/* viewing all jobs */}
+    <ViewAllJobs />
    </>
   )
 }
