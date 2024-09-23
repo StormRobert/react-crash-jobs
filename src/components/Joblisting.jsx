9b51676ee13acf0bfb9356640841a9aeff0a7dd2
StormRobert <stormarasa2@gmail.com>
2024-09-23 21:35:55 +0300

    useState added to toggle more info/description

--- src/components/Joblisting.jsx
+++ src/components/Joblisting.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import {React, useState} from 'react'
+import {FaMapMarker} from 'react-icons/fa'
 
 const Joblisting = ({job}) => {
+    const [showMoreDescription, setShowMoreDescription] = useState(false)
+
+    let description = job.description;
+
+    if(!showMoreDescription){
+        description = description.substring(0, 80) + '...';
+    }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
                 <div className="p-4">
@@ -10,8 +19,12 @@ const Joblisting = ({job}) => {
                   </div>
     
                   <div className="mb-5">
-                   {job.description}
+                   {description}
                    </div>
+
+                   <button onClick={() => setShowMoreDescription((prevState) => !prevState) } className="bg-slate-400 p-2 rounded-lg text-indigo-500 hover:text-blue-200">
+                    {showMoreDescription ? 'Show Less' : 'Show More'}
+                   </button>
     
                   <h3 className="text-indigo-500 mb-2">{job.salary}</h3>
     
@@ -19,7 +32,7 @@ const Joblisting = ({job}) => {
     
                   <div className="flex flex-col lg:flex-row justify-between mb-4">
                     <div className="text-orange-700 mb-3">
-                      <i className="fa-solid fa-location-dot text-lg"></i>
+                      <FaMapMarker className='inline text-lg mb-1 mr-1'/>
                       {job.location}
                     </div>
                     <a
